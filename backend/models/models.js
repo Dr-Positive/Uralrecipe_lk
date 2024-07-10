dc566e61f3978a2a081a12f508ee7c3204f6b7ce
Мордвинцев Даниил <mordvintsev_da@taximaxim.ru>
2024-07-10 11:27:50 +0500

    fix model

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,85 +1,78 @@
-const sequelize = require('../db')
-const {DataTypes, BelongsTo, BelongsToMany} = require('sequelize')
+const sequelize = require("../db");
+const { DataTypes, BelongsTo, BelongsToMany } = require("sequelize");
 
-const User = sequelize.define('user ', {
-    id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true}, //
-    login: {type:DataTypes.CHAR, unique: true},  // номер полиса 
-    password: {type:DataTypes.CHAR },// пока что номер договора 
-    role: {type:DataTypes.CHAR, defaultValue: 0}, // роль может иметь два значения админ или застрахованный 
-    status: {type:DataTypes.INTEGER, defaultValue: 0}, //  значение которое показывает состояние пользователя ( если заходил на сайт 1. если был проинформирован 2, а по стандарту 0)   
-    div:  {type:DataTypes.INTEGER}, // вид осмотра
-    dispt: {type:DataTypes.INTEGER}, // тема осмотра
-    div2:  {type:DataTypes.INTEGER}, // вид осмотра    
-    dispt2: {type:DataTypes.INTEGER}, // тема осмотра
-    div3:  {type:DataTypes.INTEGER}, // вид осмотра
-    dispt3: {type:DataTypes.INTEGER}, // тема осмотра    
-    fam: {type:DataTypes.CHAR}, // фамилия 
-    im:  {type:DataTypes.CHAR}, // имя
-    ot:  {type:DataTypes.CHAR}, // отчество
-    compl: {type:DataTypes.INTEGER, unique: true}, // комплект
-    gender:  {type:DataTypes.INTEGER}, // пол
+const User = sequelize.define("user ", {  // Пользователь. Информация о пользователях загруженных с базы plan_disp_m 
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }, // id пользователя с первичным ключом и автоматическим созданием(автоинкрементирование)
+  login: { type: DataTypes.CHAR, unique: true }, // номер полиса
+  password: { type: DataTypes.CHAR }, // пока что номер договора
+  role: { type: DataTypes.CHAR, defaultValue: 0 }, // роль может иметь два значения админ или застрахованный
+  status: { type: DataTypes.INTEGER, defaultValue: 0 }, //  значение которое показывает состояние пользователя ( если заходил на сайт 1. если был проинформирован 2, а по стандарту 0)
+  div: { type: DataTypes.INTEGER }, // вид осмотра
+  dispt: { type: DataTypes.INTEGER }, // тема осмотра
+  div2: { type: DataTypes.INTEGER }, // вид осмотра
+  dispt2: { type: DataTypes.INTEGER }, // тема осмотра
+  div3: { type: DataTypes.INTEGER }, // вид осмотра
+  dispt3: { type: DataTypes.INTEGER }, // тема осмотра
+  fam: { type: DataTypes.CHAR }, // фамилия
+  im: { type: DataTypes.CHAR }, // имя
+  ot: { type: DataTypes.CHAR }, // отчество
+  compl: { type: DataTypes.INTEGER, unique: true }, // комплект
+  gender: { type: DataTypes.INTEGER }, // пол
 });
 
-// const Inform = sequelize.define('inform ', {
-//     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true}, //
-//     login: {type:DataTypes.CHAR, unique: true},  // номер полиса     
-//     dispt: {type:DataTypes.INTEGER}, // тема осмотра
-//     div:  {type:DataTypes.INTEGER}, // вид осмотра
-//     fam: {type:DataTypes.CHAR}, // фамилия 
-//     im:  {type:DataTypes.CHAR}, // имя
-//     ot:  {type:DataTypes.CHAR}, // отчество
-//     compl: {type:DataTypes.INTEGER}, // комплект
-//     date: {type:DataTypes.DATE}, // комплект
-// });
-
-const Inform = sequelize.define('type_alert ', { // Проверка статуса пользователя изменяется когда пользователь заходит или получает сообщение о информировании
-    id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    alertId: {type:DataTypes.INTEGER, primaryKey: true},
-    userID: {type:DataTypes.INTEGER, primaryKey: true},
-    // flag: {type:DataTypes.INTEGER}, // значение которое передаёт состояние пользователя ( если заходил на сайт 1. если был проинформирован 2)
-    // Inform_id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    // Alert_id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    // dispt:  {type:DataTypes.INTEGER}, // тема осмотра 
-    // div:  {type:DataTypes.INTEGER},  // вид осмотра
+const Mailing = sequelize.define("mailing  ", {
+  // Рассылки. Информация о созданных рассылках
+  //Когда создаётся рассылка записывается информация о данной рассылке
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }, // id
+  createdate: { type: DataTypes.DATE }, // Дата создания информирования(с базы alert)
+  updatedate: { type: DataTypes.DATE }, // Дата последнего обновления(с базы alert)
+  title: { type: DataTypes.CHAR }, // название темы информирования(с фронта)
+  dispt: { type: DataTypes.INTEGER }, // тема осмотра(с базы alert)
+  div: { type: DataTypes.INTEGER }, // вид осмотра(с базы alert)
+  // связь между  Alert и Mailing одна запись Mailing ко многим записям в Alert
 });
 
-const Alert = sequelize.define('alert ', {       //Сообщение которое информирует пользователя о осмотре их может быть несколько
-    id: {type:DataTypes.INTEGER, primaryKey: true},
-    title:{type:DataTypes.CHAR}, // название темы информирования
-    text:{type:DataTypes.CHAR}, // текст информирования
-    dispt:  {type:DataTypes.INTEGER}, // тема осмотра 
-    div:  {type:DataTypes.INTEGER},  // вид осмотра
-    date: {type:DataTypes.DATE}, // Дата информирования
-    createdate: {type:DataTypes.DATE}, // Дата создания информирования
-    updatedate: {type:DataTypes.DATE}, // Дата последнего обновления
-    compl: {type:DataTypes.INTEGER}, // комплет-уникальный комплект
-    im:{type:DataTypes.CHAR}, // имя информированного
-    ot:{type:DataTypes.CHAR}, // отчество информированного
-    phone:{type:DataTypes.INTEGER}, // телефон информирования   
+const Alert = sequelize.define("alert ", {
+  //Сообщение. Записи всех отправленных сообщений.
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+  title: { type: DataTypes.CHAR }, // название темы информирования(с фронта)
+  dispt: { type: DataTypes.INTEGER }, // тема осмотра(с отчёта)
+  div: { type: DataTypes.INTEGER }, // вид осмотра(с фронта)
+  date: { type: DataTypes.DATE }, // Дата информирования(дата создания отчёта)
+  createdate: { type: DataTypes.DATE }, // Дата создания информирования(с фронта)
+  updatedate: { type: DataTypes.DATE }, // Дата последнего обновления(с фронта)
+  compl: { type: DataTypes.INTEGER }, // комплет-уникальный через него необходима проверка user на нахождение в базе(с отчёта) при нахождении пользователя необходима обновить у него status
+  im: { type: DataTypes.CHAR }, // имя информированного(с отчёта)
+  ot: { type: DataTypes.CHAR }, // отчество информированного(с отчёта)
+  phone: { type: DataTypes.INTEGER }, // телефон информирования(с отчёта)
+  user_id: { // id пользователя из таблицы user
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: "id",
+    },
+  },
+  mailing_id: {// id рассылки в рамках которых было информирование из таблицы Alert создаётся в момент создания рассылки
+    type: DataTypes.INTEGER,
+    references: { model: "Mailing", key: "id" },
+  },
 });
 
-User.hasOne(Alert) // Застраховнный получает сообщения 
-Alert.belongsToMany(User, {through:Inform})
 
-Alert.hasMany(User) // Отправляется сообщения
-User.belongsTo(Alert)
+// Определение связей между моделями
+User.hasMany(Alert); // У одного пользователя может быть много оповещений
+Alert.belongsTo(User); // Оповещение принадлежит конкретному пользователю
 
-Inform.hasOne(User) // Смена статуса при отправке
-Alert.belongsTo(Inform)
+// Определение связи между таблицами Mailing и Alert
+Mailing.hasMany(Alert); // У одной рассылки может быть много оповещений
+Alert.belongsTo(Mailing); // Оповещение принадлежит конкретной рассылке
 
-Inform.hasOne(User) // Смена статуса при входе
-User.belongsTo(Inform)
-
-Inform.belongsTo(User, { foreignKey: 'userId' });
-User.hasMany(Inform, { foreignKey: 'userId' });
-
-Inform.belongsTo(Alert, { foreignKey: 'alertId' });
-Alert.hasMany(Inform, { foreignKey: 'alertId' });
 
 module.exports = {
-    User, 
-    Alert,
-    Inform
-}
- // sequelize.drop()
- // sequelize.sync()
+  User,
+  Alert,
+  Mailing,
+};
+// sequelize.drop()
+// sequelize.sync()
